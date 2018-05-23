54bbe23b12fdc0e2f2c2883ea09978b24246dc11
greg-han <ghan3@mail.ccsf.edu>
2018-05-23 00:31:04 -0700

    added user 'lightup' functions, with long press functionality

--- js/script.js
+++ js/script.js
@@ -20,118 +20,11 @@ var padMap = {
   'b' : 'buttonbr',
   'y' : 'buttonbl'
 };
-var lightColorMap = {
-  'r' : 'red',
-  'g' : '#03d611',
-  'b' : 'blue',
-  'y' : 'yellow'
-}
-var regColors = {
-  'r' : '#b24242',
-  'g' : '#63b242',
-  'b' : '#4275b2',
-  'y' : '#e5e53c'
-}
-//toggles strictmode on or off.
-//will only matter if the game is in play
-
-function init(){
- turn = "simon";
-}
-
-function toggleOn(){
- if(checkbox.checked){
-  strict = true;
-  checkbox.checked = true;
- }
- else{
-  strict = false;
-  checkbox.checked = false;
- }
- console.log("strict",strict);
-}
-
-
-function selectBox(){
-  return Math.floor(Math.random()*4);
-}
-
-
-function startGame(){
- seqCount = sequence.length;
- if(!inPlay){
-  console.log(padList[selectBox()]);
- }//end of if the game is in play;
-}
-
-function restart(){
-  inPlay = false;
-  sequence = [];
-  numbers.innerHTML = '00';
-  seqCount = 0;
-}
-
-function changeTurn(){
- if(turn == 'human'){turn = 'simon';}
- else{turn = 'human';}
-}
-
-//All of the functions under this comment  control the lighting
-//of theboard by simon
-function setOff(offdoc,offcol){
- eval(offdoc).style.background = regColors[offcol];
- if(sequenceStack.length > 0){
-  populateBoard();}
- else{
-  changeTurn();
- }
-}
-
-function offPad(offcol,doci){
-  setTimeout(function(){setOff(doci,offcol);},750);
-}
-
-function lightPad(col){
-  var docID = eval(padMap[col]);
-  docID.style.background = lightColorMap[col];
-  offPad(col,docID);
-}
-
-function populateBoard(){
-  var color = sequenceStack.pop();
-  lightPad(color);
-}
-
-//end lighting functions;
-function simonTurn(){
- sequenceStack = JSON.parse(JSON.stringify(sequence));
- populateBoard();
-}
-
-
-
-
-window.onload = init;
-var padList = Object.freeze(["r","g","b","y"]);
-//Here, you can use Json.parse Json.stringify
-//or, alternatively, you can use arr.slice(0)
-var sequence = ['r','g','b','y'];
-var seuenceStack = [];
-var seqCount = 0;
-var inPlay = false;
-var strict = false;
-var turn = '';
-var checkbox = document.getElementById("switch");
-var redPad = document.getElementById("buttontl");
-var greenpad = document.getElementById("buttontr");
-var bluePad = document.getElementById("buttonbr");
-var yellowPad = document.getElementById("buttonbl");
-var numbers = document.getElementById("numtxt");
-var padMap = {
-  'r' : 'buttontl',
-  'g' : 'buttontr',
-  'b' : 'buttonbr',
-  'y' : 'buttonbl'
+var idcolMap = {
+  'buttontl' : 'r',
+  'buttontr' : 'g',
+  'buttonbr' : 'b',
+  'buttonbl' : 'y'
 };
 var lightColorMap = {
   'r' : 'red',
@@ -216,6 +109,20 @@ function populateBoard(){
 }
 
 //end lighting functions;
+
+//human lighting functions
+function padDown(id){
+  if(!inPlay){
+  eval(id).style.background = lightColorMap[idcolMap[String(id)]];
+  }
+}
+
+function padUp(divid){
+  if(!inPlay){
+  eval(divid).style.background = regColors[idcolMap[String(divid)]];
+  }
+ //put the function in here.
+}
 function simonTurn(){
  sequenceStack = JSON.parse(JSON.stringify(sequence));
  populateBoard();
